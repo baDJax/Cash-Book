85979a1fbca9f3fc55716df57309345c80a64cc7
baDJax <mukulsharma262002@gmail.com>
2024-09-24 16:11:21 +0530

    Updated layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${livvic.className} antialiased`}
-      >
+      <body className={`${livvic.className} antialiased bg-background`}>
         {children}
       </body>
     </html>
