1741124d6bb03a1e49cd8c460b7d670d30dd45d6
baDJax <mukulsharma262002@gmail.com>
2024-09-24 17:58:24 +0530

    Updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,16 @@ const config: Config = {
         "gradient-red": "#EB001B",
         "gradient-blue": "#0042FF",
       },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "1.5rem",
+          lg: "2rem",
+          xl: "2.5rem",
+          "2xl": "3rem",
+        },
+      },
     },
   },
   plugins: [],
