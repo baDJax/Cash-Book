c7e788e894931cdc05bde1f997d7386972a543dd
baDJax <mukulsharma262002@gmail.com>
2024-09-24 16:13:00 +0530

    Added dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Dashboard from "@/components/Dashboard";
+import Sidebar from "@/components/Sidebar";
+
 export default function Home() {
-  return <></>;
+  return (
+    <section className="">
+      <Sidebar />
+      <Dashboard />
+    </section>
+  );
 }
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CashBoard from "./CashBoard";
+
+const Dashboard = () => {
+  return (
+    <div className="ps-[88px]">
+      <div className="p-7">
+        <CashBoard />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
