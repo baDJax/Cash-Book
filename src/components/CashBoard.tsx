105628af251e03c3dae9bd902191f11be61ef423
baDJax <mukulsharma262002@gmail.com>
2024-09-30 18:09:45 +0530

    Updated button and container styling in cashboard

--- src/components/CashBoard.tsx
+++ src/components/CashBoard.tsx
@@ -5,7 +5,7 @@ import Profile from "#/profile.png";
 
 const CashBoard = () => {
   return (
-    <div className="flex p-4 w-full h-full bg-gradient-to-bl rounded-xl from-gradient-red to-gradient-blue">
+    <div className="col-span-2 flex justify-between p-4 w-full h-full bg-gradient-to-bl rounded-xl from-gradient-red to-gradient-blue">
       <div className="max-w-[438px]">
         <Image
           className="-translate-x-8 translate-y-6 scale-110"
@@ -13,7 +13,7 @@ const CashBoard = () => {
           alt="Dashboard Image"
         />
       </div>
-      <div className="w-full flex flex-col">
+      <div className="w-full flex flex-col max-w-[450px]">
         <div className="flex items-center justify-between">
           <h2 className="text-3xl font-bold">Investments</h2>
           <div className="flex items-center">
@@ -54,10 +54,10 @@ const CashBoard = () => {
           </div>
         </div>
         <div className="flex mt-auto gap-4 font-semibold">
-          <button className="transition-all bg-white/70 text-black hover:bg-white py-2 px-4 rounded">
+          <button className="w-full transition-all bg-white/70 text-black hover:bg-white py-2 px-4 rounded">
             View Rewards
           </button>
-          <button className="transition-all bg-transparent border border-white text-white hover:text-white/70 hover:border-white/10 hover:bg-gradient-to-bl to-gradient-red/40 from-gradient-blue/40 py-2 px-4 rounded">
+          <button className="w-full transition-all bg-transparent border border-white text-white hover:text-white/70 hover:border-white/10 hover:bg-gradient-to-bl to-gradient-red/40 from-gradient-blue/40 py-2 px-4 rounded">
             View Collection
           </button>
         </div>
@@ -67,4 +67,3 @@ const CashBoard = () => {
 };
 
 export default CashBoard;
-
